9753d05c4650a42b209ba51b639a141e1f79cf74
Valentina Varela Alzate <valentina.varela@utp.edu.co>
2022-07-25 18:28:27 -0500

    resolucion ejercicios

---
+++ OpenBootcamp/Tema3/PrimeraParte/suma.java
@@ -0,0 +1,13 @@
+package OpenBootcamp.Tema3.PrimeraParte;
+
+public class suma {
+    public int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String args[]) {
+        suma c = new suma();
+        System.out.println(c.sumar(2, 2, 2));
+
+    }
+}
